c44fb35b3e7dcd99d859a01e4c222582a1626f08
Blickfeldkurier <Blickfeldkurier@posteo.de>
2018-05-04 21:19:06 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Mastposd
+Daemon to print Mastodon Toots on a Thermal Printer
+
+## Dependencies
+
+## Installation
+
+## Configuration
---
+++ mastposd.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  4 21:18:13 2018
+
+@author: blickfeldkurier
+"""
+
