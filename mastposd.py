9d89a2d811122a6c9bfe13ec11755f60875b2c78
Blickfeldkurier <Blickfeldkurier@posteo.de>
2018-05-04 21:52:59 +0200

    Function for Debug and Error Printing added

--- mastposd.py
+++ mastposd.py
@@ -5,8 +5,16 @@ Created on Fri May  4 21:18:13 2018
 
 @author: blickfeldkurier
 """
+import syslog
 import argparse
 import daemon
+
+def printmsg(args, message, priority=syslog.LOG_INFO):
+    """Print a message to Console or Syslog"""
+    if args.debug is True:
+        print(message)
+    else:
+        syslog.syslog(priority, message)
 
 def main(args):
     """Connect to Printer and Watch Mastodon"""
